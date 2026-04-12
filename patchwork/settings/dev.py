edf8a85ae1086e032f31b56d74007bf59618a135
Franciszek Stachura <fbstachura@gmail.com>
2026-04-12 16:36:07 +0200

    WIP: add all addresses of my podman internal network to INTERNAL_IPS

--- patchwork/settings/dev.py
+++ patchwork/settings/dev.py
@@ -63,6 +63,8 @@ if debug_toolbar:
     ] + MIDDLEWARE  # noqa: F405
 
     INTERNAL_IPS = ['127.0.0.1', '::1', '172.18.0.1']
+    for n in range(2, 254):
+        INTERNAL_IPS.append(f'10.89.0.{n}')
 
 # django-dbbackup
 
